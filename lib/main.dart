7c886d209a5c17278afff5bef7e0a55bed8b8d8b
TJ-numan <abdullahalnuman235@gmail.com>
2021-07-06 19:00:21 +0600

    widget_test file edited

--- lib/main.dart
+++ lib/main.dart
@@ -21,16 +21,12 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       body: Center(
-        
-        child:  Text ("This is not the body",
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'indieFlower',
-              letterSpacing: 1.0,
-              color: Colors.blueAccent
+
+        child:  Image(
+          image: NetworkImage(
+              "https://images.unsplash.com/photo-1542144612-1b3641ec3459?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80"
           ),
-        ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
